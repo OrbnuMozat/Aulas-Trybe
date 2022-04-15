016c411e9dde1b3728bafeab1a585c7edf0ffcb5
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-04-14 23:59:38 -0300

    Exercício do Conteúdo Pt-3

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -6,8 +6,6 @@ let textFilhoFilho = document.getElementById('primeiroFilhoDoFilho');
 
 textFilhoFilho.innerText = "Teste";
 
-document.getElementById('elementoOndeVoceEsta').appendChild = textFilhoFilho;
-
 console.log(document.getElementById('pai').firstElementChild);
 
 console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
@@ -17,3 +15,47 @@ console.log(document.getElementById('elementoOndeVoceEsta').parentNode.innerText
 console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
 
 console.log(document.getElementById('pai').firstElementChild.nextElementSibling.nextElementSibling);
+
+let newArticle = document.createElement('section');
+
+newArticle.id = 'brother'
+
+document.getElementById('elementoOndeVoceEsta').parentNode.appendChild(newArticle);
+
+console.log(document.getElementById('brother'));
+
+let neto = document.createElement('section');
+
+neto.id = 'neto';
+
+document.getElementById('elementoOndeVoceEsta').appendChild(neto);
+
+console.log(document.getElementById('neto'));
+
+let bisneto = document.createElement('section');
+
+bisneto.id = 'bisneto';
+
+document.getElementById('primeiroFilhoDoFilho').appendChild(bisneto);
+
+console.log(document.getElementById('bisneto'));
+
+console.log(document.getElementById('bisneto').parentNode.parentNode.nextElementSibling);
+
+let pai = document.getElementById('pai');
+
+pai.removeChild(document.getElementById('brother'));
+
+console.log(pai);
+
+pai.removeChild(document.getElementById('primeiroFilho'));
+
+pai.removeChild(document.getElementById('terceiroFilho'));
+
+pai.removeChild(document.getElementById('quartoEUltimoFilho'));
+
+let ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+ondeVoceEsta.removeChild(document.getElementById('segundoEUltimoFilhoDoFilho'));
+
+ondeVoceEsta.removeChild(document.getElementById('neto'));
