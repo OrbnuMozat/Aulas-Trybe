ac96a897cb52765345da280fa1da458d3a1fa50e
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-13 20:15:58 -0300

    exercicio 1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-introducao-higher-order-functions/exercicios-8.1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-introducao-higher-order-functions/exercicios-8.1.js
@@ -4,19 +4,19 @@ Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa co
 
 A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.*/
 
-const registerEmployess = (name, employeeEmail) => (
+const registerEmployess = (name) => (
     {
         nomeCompleto: name,
-        email: employeeEmail
+        email: `${name.replace(' ', '_').toLowerCase()}@trybe.com`,
     }
 );
 
 
 const newEmployees = (callback) => {
     const employees = {
-      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'),
     }
     return employees;
   };
