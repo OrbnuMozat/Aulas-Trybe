dbc3eb5187cdabf243233f4d56c0409901f1600f
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-13 18:09:35 -0300

    first commit :rocket:

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-introducao-higher-order-functions/exercicios-8.1.js
@@ -0,0 +1,9 @@
+// Exercício do Conteúdo
+const awake = () => 'Acordando!!';
+const coffe = () => 'Bora tomar café';
+const zzzZ = () => 'Partiu dormir';
+
+const doingThings = (thing) => {console.log(thing())};
+
+doingThings(awake);
+
