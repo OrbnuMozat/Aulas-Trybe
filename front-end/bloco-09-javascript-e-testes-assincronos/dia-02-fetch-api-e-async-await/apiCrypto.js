84738935bb14ff340ca93f5f8ae9ea89a90c6a73
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-06-02 16:11:44 -0300

    exercicios completos 9.2

--- front-end/bloco-09-javascript-e-testes-assincronos/dia-02-fetch-api-e-async-await/apiCrypto.js
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-02-fetch-api-e-async-await/apiCrypto.js
@@ -1,32 +1,53 @@
-const API_CRYPTO = `https://api.coincap.io/v2/assets`;
+const createUl = (id) => {
+    const container = document.getElementById('cryptoContainer')
+    const ul = document.createElement('ul');
+    ul.id = id;
+    container.appendChild(ul);
+};
 
-const insertInHtml = (cryptoMoeda) => {
+const insertCryptoInUl = (cryptoMoeda) => {
     const cryptoList = document.getElementById('cryptoList');
     const newLi = document.createElement('li');
     newLi.innerHTML = cryptoMoeda;
     cryptoList.appendChild(newLi);
-}
-
-const prepareCryptoList = (dataArrayCrypto) => {
-    const filterTenFirstCrypto = dataArrayCrypto.slice(0, 10);
-    filterTenFirstCrypto.map((crypto) => {
-        const { name, symbol, priceUsd } = crypto;
-        const priceUsd2Dec = Number(priceUsd).toFixed(2);
-        const text = `${name} (${symbol}) ${priceUsd2Dec}`;
-        return insertInHtml(text);
-    });
 };
 
-const fetchCrypto = () => {
-    const objCrypto = {
-        method: 'GET',
-        headers: { 'Accept-Encoding': 'deflate' }
-    };
 
-    fetch(API_CRYPTO, objCrypto)
+const fetchCrypto = async () => {
+    const url = `https://api.coincap.io/v2/assets`;
+    
+    const crypto = await fetch(url)
+    .then(response => response.json())
+    .then(dataArray => dataArray.data)
+    .then(arrayCrypto => prepareCryptoList(arrayCrypto))
+    .catch((error) => error.toString());
+    return crypto;
+};
+
+const fetchCurrencies = async () => {
+    const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
+
+    const brlCurrencies = await fetch(url)
         .then(response => response.json())
-        .then(dataArray => dataArray.data)
-        .then(arrayCrypto => prepareCryptoList(arrayCrypto));
+        .then(data => data.usd)
+        .then(currencies => currencies.brl)
+        .catch((error) => error.toString());
+    return brlCurrencies;
+};
+
+const prepareCryptoList = async (dataArrayCrypto) => {
+    const filterTenFirstCrypto = dataArrayCrypto.slice(0, 10);
+    const currencies = await fetchCurrencies();
+    createUl('cryptoList');
+    filterTenFirstCrypto.map((crypto) => {
+        const { name, symbol, priceUsd } = crypto;
+        const priceUsdNumber = Number(priceUsd);
+        const priceBrlNumber = Number(currencies);
+        const priceBrl = priceUsdNumber * priceBrlNumber;
+        const priceBrl2Dec = priceBrl.toFixed(2);
+        const text = `${name} (${symbol}) - R$ ${priceBrl2Dec}`;
+        return insertCryptoInUl(text);
+    });
 };
 
 window.onload = () => fetchCrypto();
