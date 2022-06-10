46d3c729a4d7c4ac4024f0b12a28a606a507c9d7
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-06-10 18:30:45 -0300

    first commit

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/testando/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactClass from './Componentes/primeiroComponente';
+
+function App() {
+  const textJSX = 'Hello, JSX';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{textJSX}</h1>
+        < ReactClass />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/testando/src/Componentes/primeiroComponente.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ReactClass extends React.Component {
+    render () {
+        return (
+            <h1>Meu primeiro componente react!</h1>
+        )
+    }
+}
+
+export default ReactClass;
