4759b9cf43522f0434574bc29d06d8e6a68a7f0e
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-08-17 14:07:20 -0300

    firts commit

---
+++ front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useeffect-e-hooks-customizados/todo-list/src/Routes.js
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path='/' exact component={Login} />
+    </Switch>
+  )
+}
+
+export default Routes;
